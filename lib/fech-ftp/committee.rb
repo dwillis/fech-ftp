bd552dca2917aa7ac7579d74819ebfd536c7e30c
Derek Willis <dwillis@gmail.com>
2014-01-16 10:43:45 -0500

    renamed Committee#pac_summary to summary

--- lib/fech-ftp/committee.rb
+++ lib/fech-ftp/committee.rb
@@ -12,7 +12,7 @@ module Fech
     end
     
     # http://www.fec.gov/finance/disclosure/metadata/DataDictionaryWEBK.shtml
-    def self.pac_summary(cycle)
+    def self.summary(cycle)
       cols = [:committee_id, :committee_name, :type, :designation, :filing_frequency, :total_receipts, :transfers_from_affiliates, :individual_contributions, :pac_contributions, :candidate_contributions, 
         :candidate_loans, :total_loans_received, :total_disbursements, :transfers_to_affiliates, :individual_refunds, :committee_refunds, :candidate_loan_repayments, :loan_repayments, :beginning_year_cash, 
         :ending_cash, :debts_owed_by, :nonfederal_transfers_received, :contributions_to_committees, :independent_expenditures, :party_coordinated_expenditures, :nonfederal_share_expenditures, :coverage_end_date]
