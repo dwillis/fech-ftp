485370b7953dd259ffaa692ac12d158b2cf4c456
wismer <matthewhl@gmail.com>
2014-06-10 00:12:22 -0400

    add simple one stop-shop command for retrieving fec data

--- lib/fech-ftp.rb
+++ lib/fech-ftp.rb
@@ -1,16 +1,35 @@
 require 'fech'
 require "fech-ftp/version"
+require "fech-ftp/ftp"
 require "fech-ftp/committee"
 require "fech-ftp/candidate"
 require "fech-ftp/candidate_contribution"
 require "fech-ftp/committee_contribution"
+require "fech-ftp/headers"
+require "net/ftp"
+require 'zip'
 require 'remote_table'
 require 'american_date'
 require 'csv'
+require 'pry'
 
 module Fech
-  class Utilities
+  def self.retrieve_table(table, cycle, opts={})
+    table = case table
+            when :candidate_contribution
+              CandidateContribution.new(cycle)
+            when :candidate
+              Candidate.new(cycle, opts[:type])
+            when :committee
+              Committee.new(cycle, opts[:type])
+            when :committee_contribution
+              CommitteeContribution.new(cycle, opts[:type])
+            end
 
+    table.fetch
+  end
+
+  class Utilities
     def self.write_to_csv(filename, headers, rows)
       CSV.open("#{filename}.csv", "w") do |csv|
         csv << headers
