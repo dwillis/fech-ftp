0d38ec5bd1ddf1bfc3ed7a15e41a38fe1de49c81
Matt Long <matthewhl@gmail.com>
2014-07-27 19:36:43 -0400

    subclass of table

--- lib/fech-ftp/candidate_contribution.rb
+++ lib/fech-ftp/candidate_contribution.rb
@@ -1,43 +1,13 @@
 module Fech
-  class CandidateContribution
-
-    def self.load(cycle)
-      cols = [:committee_id, :amendment, :report_type, :primary_general, :microfilm, :transaction_type, :entity_type, :name, :city, :state, :zipcode, 
-      	:employer, :occupation, :date, :amount, :other_committee_id, :candidate_id, :transaction_id, :filing_id, :memo_code, :memo_text, :fec_record_number]
-      url = "ftp://ftp.fec.gov/FEC/#{cycle}/pas2#{cycle.to_s[2..3]}.zip"
-      t = RemoteTable.new url, :filename => "itpas2.txt", :col_sep => "|", :headers => cols
-      rows = t.entries
-      rows.each do |row|
-        row.merge!({
-          committee_id: row[:committee_id], 
-          amendment: row[:amendment],
-          report_type: row[:report_type], 
-          primary_general: row[:primary_general],
-          microfilm: row[:microfilm],
-          transaction_type: row[:transaction_type],
-          entity_type: row[:entity_type],
-          name: row[:name],
-          city: row[:city], 
-          state: row[:state],
-          zipcode: row[:zipcode],
-          employer: row[:employer],
-          occupation: row[:occupation],
-          date: begin Date.strptime(row[:date], "%m%d%Y") rescue row[:date] end,
-          amount: row[:amount].to_f,
-          other_committee_id: row[:other_committee_id],
-          candidate_id: row[:candidate_id],
-          transaction_id: row[:transaction_id],
-          filing_id: row[:filing_id].to_i,
-          memo_code: row[:memo_code],
-          memo_text: row[:memo_text],
-          fec_record_number: row[:fec_record_number]
-         })
-      end
-      rows
-    end
-
-    
-
-
+  class CandidateContribution < Table
+    HEADERS = {
+      file: "oth",
+      headers: [
+        :committee_id, :amendment, :report_type, :primary_general, :microfilm,
+        :transaction_type, :entity_type, :name, :city, :state, :zipcode,
+        :employer, :occupation, :transaction_date, :amount, :other_committee_id, :candidate_id,
+        :transaction_id, :filing_id, :memo_code, :memo_text, :fec_record_number
+      ]
+    }
   end
 end
--- lib/fech-ftp/committee.rb
+++ lib/fech-ftp/committee.rb
@@ -1,88 +1,35 @@
 module Fech
-  class Committee
-
-    DETAIL_COLS = [:committee_id, :committee_name, :treasurer, :street_one, :street_two, :city, :state, :zipcode, :designation, :type, :party, :filing_frequency, :category, :connected_organization, :candidate_id]
-    SUMMARY_COLS = [:committee_id, :committee_name, :type, :designation, :filing_frequency, :total_receipts, :transfers_from_affiliates, :individual_contributions, :pac_contributions, :candidate_contributions, 
-        :candidate_loans, :total_loans_received, :total_disbursements, :transfers_to_affiliates, :individual_refunds, :committee_refunds, :candidate_loan_repayments, :loan_repayments, :beginning_year_cash, 
-        :ending_cash, :debts_owed_by, :nonfederal_transfers_received, :contributions_to_committees, :independent_expenditures, :party_coordinated_expenditures, :nonfederal_share_expenditures, :coverage_end_date]
-    LINKAGE_COLS = [:candidate_id, :candidate_election_year, :election_year, :committee_id, :type, :designation, :linkage_id]
-
-    def self.write_to_csv(method, cycle)
-      headers = method == 'detail' ? DETAIL_COLS : SUMMARY_COLS
-      rows = self.send(method, cycle)
-      Fech::Utilities.write_to_csv("committees_#{method}_#{cycle}", headers, rows)
-    end
-
-    # corresponds to the FEC Committee master file described here:
-    # http://www.fec.gov/finance/disclosure/metadata/DataDictionaryCommitteeMaster.shtml
-    def self.detail(cycle)
-      url = "ftp://ftp.fec.gov/FEC/#{cycle}/cm#{cycle.to_s[2..3]}.zip"
-      t = RemoteTable.new url, :filename => "cm.txt", :col_sep => "|", :headers => DETAIL_COLS
-      t.entries
-    end
-    
-    # http://www.fec.gov/finance/disclosure/metadata/DataDictionaryWEBK.shtml
-    def self.summary(cycle)
-      url = "ftp://ftp.fec.gov/FEC/#{cycle}/webk#{cycle.to_s[2..3]}.zip"
-      t = RemoteTable.new url, :filename => "webk#{cycle.to_s[2..3]}.txt", :col_sep => "|", :headers => SUMMARY_COLS
-      rows = t.entries
-      rows.each do |row|
-        row.merge!({
-          committee_id: row[:committee_id], 
-          committee_name: row[:committee_name],
-          type: row[:type], 
-          designation: row[:designation],
-          filing_frequency: row[:filing_frequency],
-          total_receipts: row[:total_receipts].to_f,
-          transfers_from_affiliates: row[:transfers_from_affiliates].to_f,
-          individual_contributions: row[:individual_contributions].to_f,
-          pac_contributions: row[:pac_contributions].to_f, 
-          candidate_contributions: row[:candidate_contributions].to_f,
-          candidate_loans: row[:candidate_loans].to_f,
-          total_loans_received: row[:total_loans_received].to_f,
-          total_disbursements: row[:total_disbursements].to_f,
-          transfers_to_affiliates: row[:transfers_to_affiliates].to_f,
-          individual_refunds: row[:individual_refunds].to_f, 
-          committee_refunds: row[:committee_refunds].to_f, 
-          candidate_loan_repayments: row[:candidate_loan_repayments].to_f,
-          loan_repayments: row[:loan_repayments].to_f,
-          beginning_year_cash: row[:beginning_year_cash].to_f,
-          ending_cash: row[:ending_cash].to_f,
-          debts_owed_by: row[:debts_owed_by].to_f,
-          nonfederal_transfers_received: row[:nonfederal_transfers_received].to_f, 
-          contributions_to_committees: row[:contributions_to_committees].to_f,
-          independent_expenditures: row[:independent_expenditures].to_f,
-          party_coordinated_expenditures: row[:party_coordinated_expenditures].to_f,
-          nonfederal_share_expenditures: row[:nonfederal_share_expenditures].to_f,
-          coverage_end_date: begin Date.parse(row[:coverage_end_date]) rescue row[:coverage_end_date] end
-         })
-      end
-      rows
-    end
-
-    def self.linkages(cycle)
-      url = "ftp://ftp.fec.gov/FEC/#{cycle}/ccl#{cycle.to_s[2..3]}.zip"
-      t = RemoteTable.new url, :filename => "ccl.txt", :col_sep => "|", :headers => LINKAGE_COLS
-      rows = t.entries
-      rows.each do |row|
-        row.merge!({
-          candidate_id: row[:candidate_id],
-          candidate_election_year: row[:candidate_election_year],
-          election_year: row[:election_year],
-          committee_id: row[:committee_id], 
-          type: row[:type], 
-          designation: row[:designation],
-          linkage_id: row[:linkage_id]
-        })
-      end
-      rows
-    end
-
-    def self.load_superpacs
-      url = "http://www.fec.gov/press/press2011/ieoc_alpha.shtml"
-      t = RemoteTable.new url, :row_xpath => '//table/tr', :column_xpath => 'td', :encoding => 'windows-1252', :headers => %w{ row_id committee_id committee_name filing_frequency}
-      t.entries
-    end
-
+  class Committee < Table
+    HEADERS = {
+      detail: {
+        file: 'cm',
+        headers: [
+          :committee_id, :committee_name, :treasurer, :street_one,
+          :street_two,:city, :state, :zipcode, :designation, :type, :party,
+          :filing_frequency, :category, :connected_organization, :candidate_id
+        ]
+      },
+      summary_all: {
+        file: 'webk',
+        headers: [
+          :committee_id, :committee_name, :type, :designation, :filing_frequency,
+          :total_receipts, :transfers_from_affiliates, :individual_contributions,
+          :pac_contributions, :candidate_contributions, :candidate_loans,
+          :total_loans_received, :total_disbursements, :transfers_to_affiliates,
+          :individual_refunds, :committee_refunds, :candidate_loan_repayments,
+          :loan_repayments, :beginning_year_cash, :ending_cash, :debts_owed_by,
+          :nonfederal_transfers_received, :contributions_to_committees,
+          :independent_expenditures, :party_coordinated_expenditures,
+          :nonfederal_share_expenditures, :coverage_end_date
+        ]
+      },
+      linkage: {
+        file: 'ccl',
+        headers: [
+          :committee_id, :candidate_election_year, :election_year,
+          :candidate_id, :type, :designation, :linkage_id
+        ]
+      }
+    }
   end
 end
