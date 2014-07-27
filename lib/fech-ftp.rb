a5dde7fb8638a4fdb86e712347e7513db4fb7d57
Matt Long <matthewhl@gmail.com>
2014-07-27 19:38:47 -0400

    remove csv logic; move superpac from committee class

--- lib/fech-ftp.rb
+++ lib/fech-ftp.rb
@@ -1,21 +1,24 @@
 require 'fech'
 require "fech-ftp/version"
+require "fech-ftp/table"
 require "fech-ftp/committee"
 require "fech-ftp/candidate"
 require "fech-ftp/candidate_contribution"
 require "fech-ftp/committee_contribution"
+require "fech-ftp/individual_contribution"
+require "fech-ftp/table_methods"
+require "net/ftp"
+require 'zip'
 require 'remote_table'
 require 'american_date'
 require 'csv'
 
 module Fech
   class Utilities
-
-    def self.write_to_csv(filename, headers, rows)
-      CSV.open("#{filename}.csv", "w") do |csv|
-        csv << headers
-        rows.map{|r| csv << r.values}
-      end
+    def self.superpacs
+      url = "http://www.fec.gov/press/press2011/ieoc_alpha.shtml"
+      t = RemoteTable.new url, :row_xpath => '//table/tr', :column_xpath => 'td', :encoding => 'windows-1252', :headers => %w{ row_id committee_id committee_name filing_frequency}
+      t.entries
     end
   end
 end
