efe9e7a369a2388503aa56ec4eef63eefb262716
Matt Long <matthewhl@gmail.com>
2014-07-28 21:23:22 -0400

    remove misplaced candidate_id header

--- lib/fech-ftp/committee_contribution.rb
+++ lib/fech-ftp/committee_contribution.rb
@@ -5,7 +5,7 @@ module Fech
       headers: [
         :committee_id, :amendment, :report_type, :primary_general, :microfilm,
         :transaction_type, :entity_type, :name, :city, :state, :zipcode,
-        :employer, :occupation, :transaction_date, :amount, :other_committee_id, :candidate_id,
+        :employer, :occupation, :transaction_date, :amount, :other_committee_id,
         :transaction_id, :filing_id, :memo_code, :memo_text, :fec_record_number
       ]
     }
