0971e1bb0c804134134b4a95d4435b743b1b6a55
Derek Willis <dwillis@nytimes.com>
2015-03-18 10:45:41 -0400

    added detail method for committee contribution

--- lib/fech-ftp/committee_contribution.rb
+++ lib/fech-ftp/committee_contribution.rb
@@ -1,13 +1,15 @@
 module Fech
   class CommitteeContribution < Table
     HEADERS = {
-      file: "oth",
-      headers: [
-        :committee_id, :amendment, :report_type, :primary_general, :microfilm,
-        :transaction_type, :entity_type, :name, :city, :state, :zipcode,
-        :employer, :occupation, :transaction_date, :amount, :other_committee_id,
-        :transaction_id, :filing_id, :memo_code, :memo_text, :fec_record_number
-      ]
+      detail: {
+        file: "oth",
+        headers: [
+          :committee_id, :amendment, :report_type, :primary_general, :microfilm,
+          :transaction_type, :entity_type, :name, :city, :state, :zipcode,
+          :employer, :occupation, :transaction_date, :amount, :other_committee_id,
+          :transaction_id, :filing_id, :memo_code, :memo_text, :fec_record_number
+        ]
+      }
     }
   end
 end
