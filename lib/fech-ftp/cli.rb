f7dfa2c0e1aa6de5eacb9d8f8c18aae47563c90f
Matt Long <matthewhl@gmail.com>
2015-05-07 09:59:08 -0400

    reduce methods to single use - download

--- lib/fech-ftp/cli.rb
+++ lib/fech-ftp/cli.rb
@@ -1,8 +1,16 @@
 module Fech
   class Ftp < Thor
+    include Dispatcher
+
     desc "Download individual records to <DIR> by specified election year", "Download individual records by year"
-    def individual(year, dir)
-      
+    # fech-ftp download individual 2012 --detail=format:csv
+    # fech-ftp download committee -c 2012 --format=csv
+    method_option :format, :type => :string, :require => true, :alias => "-f"
+    method_option :contributions, :alias => "-c", :default => ""
+
+    def download(table_name="", meth="")
+      table = dataset(table_name, options[:contributions])
+      table.send(meth, options)
     end
   end
 end
