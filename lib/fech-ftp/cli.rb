cf7100a06b184ca3cde3cc4b63bfbe2afdefbb8e
Matt Long <matthewhl@gmail.com>
2015-05-07 11:35:18 -0400

    reassign keys to symbols

--- lib/fech-ftp/cli.rb
+++ lib/fech-ftp/cli.rb
@@ -1,16 +1,24 @@
+require 'thor'
+require 'pry'
 module Fech
-  class Ftp < Thor
-    include Dispatcher
-
-    desc "Download individual records to <DIR> by specified election year", "Download individual records by year"
+  class CLI < Thor
+    include Fech::Dispatcher
     # fech-ftp download individual 2012 --detail=format:csv
     # fech-ftp download committee -c 2012 --format=csv
+    desc "Download individual records to <DIR> by specified election year", "Download individual records by year"
     method_option :format, :type => :string, :require => true, :alias => "-f"
     method_option :contributions, :alias => "-c", :default => ""
+    method_option :year, :type => :numeric, :require => true, :alias => "-y"
 
     def download(table_name="", meth="")
       table = dataset(table_name, options[:contributions])
-      table.send(meth, options)
+      opts = options.inject({}){|opt,(k,v)| opt[k.to_sym] = v; opt}
+      opts[:format] = opts[:format].to_sym
+      if table.respond_to? meth
+        table.send(meth, options[:year], opts)
+      else
+        raise 'invalid method'
+      end
     end
   end
 end
