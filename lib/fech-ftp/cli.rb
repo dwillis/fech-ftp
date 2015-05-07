781e00db5942540fe0c368496608c056474f907c
Matt Long <matthewhl@gmail.com>
2015-05-06 21:22:11 -0400

    first stages of cli

---
+++ lib/fech-ftp/cli.rb
@@ -0,0 +1,8 @@
+module Fech
+  class Ftp < Thor
+    desc "Download individual records to <DIR> by specified election year", "Download individual records by year"
+    def individual(year, dir)
+      
+    end
+  end
+end
