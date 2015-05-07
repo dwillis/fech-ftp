32981d603ab33b2466a8561a0009d78bcffc47b9
Matt Long <matthewhl@gmail.com>
2015-05-07 09:07:49 -0400

    add dispatcher methods

---
+++ lib/fech-ftp/dispatcher.rb
@@ -0,0 +1,16 @@
+module Fech
+  module Dispatcher
+    def download(params={})
+
+    end
+
+    def dataset(table, subset='')
+      table = "#{table.capitalize}#{subset.capitalize}"
+      if Fech.const_defined?(table)
+        Fech.const_get("#{table.capitalize}#{subset.capitalize}")
+      else
+        raise 'Invalid table'
+      end
+    end
+  end
+end
